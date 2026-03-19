41cd96f6bc05cc05dab66f52b96350a4fdbbf049
suyeonTech <wsy-ofc@naver.com>
2026-03-19 14:18:22 +0900

    feat: MemberController, MemberRepository, MemberService, AuthController 생성 및 기본코드 세팅

---
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.back.domain.member.member.service;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+}
