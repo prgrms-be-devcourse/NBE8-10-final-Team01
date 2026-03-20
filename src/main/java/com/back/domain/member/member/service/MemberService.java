3c68aa4dde2e8042fcd3863eddb2d2812b3912f0
suyeonTech <wsy-ofc@naver.com>
2026-03-20 16:49:56 +0900

    비밀번호 유효성검사 및 비밀번호 확인절차 추가

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -33,6 +33,17 @@ public class MemberService {
             throw new ServiceException("MEMBER_400", "닉네임은 필수 입력값입니다");
         }
 
+        // 비밀번호 확인 일치 검증
+        if (!req.password().equals(req.passwordConfirm())) {
+            throw new ServiceException("MEMBER_400", "비밀번호와 비밀번호 확인이 일치하지 않습니다");
+        }
+
+        // 비밀번호 정책 검증 (8~12자, 영문+숫자+특수문자 모두 포함)
+        String passwordPattern = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,12}$";
+        if (!req.password().matches(passwordPattern)) {
+            throw new ServiceException("MEMBER_400", "비밀번호는 8~12자이며 영문, 숫자, 특수문자를 모두 포함해야 합니다");
+        }
+
         // 이메일 중복 체크
         if (memberRepository.existsByEmail(req.email())) {
             throw new ServiceException("MEMBER_409", "이미 존재하는 이메일입니다");
@@ -44,8 +55,8 @@ public class MemberService {
         // 회원 생성 (엔티티 팩토리 메서드 사용)
         Member member = Member.createUser(req.name(), req.email(), encodedPassword);
 
-        Member savedMember = memberRepository.save(member);
+        memberRepository.save(member);
 
-        return RsData.of("200", "");
+        return RsData.of("200", "회원가입성공");
     }
 }
