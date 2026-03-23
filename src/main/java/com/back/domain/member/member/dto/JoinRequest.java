70359891d4fdad2288a456bd5e81b4f05b3fb7da
suyeonTech <wsy-ofc@naver.com>
2026-03-23 09:47:34 +0900

    style: spotless Apply

--- src/main/java/com/back/domain/member/member/dto/JoinRequest.java
+++ src/main/java/com/back/domain/member/member/dto/JoinRequest.java
@@ -7,22 +7,13 @@ import jakarta.validation.constraints.Size;
 
 // 회원가입 요청 바디(JSON) 구조
 public record JoinRequest(
-        @NotBlank(message = "이메일은 필수 입력값입니다")
-        @Email(message = "올바른 이메일 형식이 아닙니다")
-        String email,
+        @NotBlank(message = "이메일은 필수 입력값입니다") @Email(message = "올바른 이메일 형식이 아닙니다") String email,
 
-        @NotBlank(message = "비밀번호는 필수 입력값입니다")
-        @Pattern(
+        @NotBlank(message = "비밀번호는 필수 입력값입니다") @Pattern(
                 regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,12}$",
-                message = "비밀번호는 8~12자이며 영문, 숫자, 특수문자를 모두 포함해야 합니다"
-        )
+                message = "비밀번호는 8~12자이며 영문, 숫자, 특수문자를 모두 포함해야 합니다")
         String password,
 
-        @NotBlank(message = "비밀번호 확인은 필수입니다")
-        String passwordConfirm,
+        @NotBlank(message = "비밀번호 확인은 필수입니다") String passwordConfirm,
 
-        @NotBlank(message = "닉네임은 필수 입력값입니다")
-        @Size(min = 2, max = 20, message = "닉네임은 2~20자 사이여야 합니다")
-        String name
-) {
-}
+        @NotBlank(message = "닉네임은 필수 입력값입니다") @Size(min = 2, max = 20, message = "닉네임은 2~20자 사이여야 합니다") String name) {}
