6d028d939b5043fe08ab61867c593647b32eb589
suyeonTech <wsy-ofc@naver.com>
2026-04-03 19:16:14 +0900

    fix: add spring-boot-starter-actuator dependency for health and metrics endpoints

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
